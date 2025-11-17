37229b1580054b93126d9fe9d103e3d572c73bbc
talipbozkurt <talipbozkurt19@gmail.com>
2025-11-17 16:41:48 +0300

    feat: add formatted print example

---
+++ src/Strings/PrettyPrint.java
@@ -0,0 +1,10 @@
+package Strings;
+
+public class PrettyPrint {
+    public static void main(String[] args) {
+        float num = 5154.494949f;
+        System.out.printf(" Formatted number is %.2f \n",num);
+
+        System.out.printf(" Pie : %.4f \n",Math.PI);
+    }
+}
